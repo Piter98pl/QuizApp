0647e2a2472ea3c6f169ca1ddd073037ad189aae
Piotr Orzechowski <piotrekorzech7@gmail.com>
2022-02-21 12:45:18 +0100

    final property

--- lib/question.dart
+++ lib/question.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   Question(
       this.questionText); //short form,  this receives some input data because it gets some data in its constructor
