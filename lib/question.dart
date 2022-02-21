9e4ab17e40752f14233f0aa7a1cdefc5f5f14148
Piotr Orzechowski <piotrekorzech7@gmail.com>
2022-02-21 21:27:08 +0100

    Added styling to question text

--- lib/question.dart
+++ lib/question.dart
@@ -4,10 +4,20 @@ class Question extends StatelessWidget {
   final String questionText;
 
   Question(
-      this.questionText); //short form,  this receives some input data because it gets some data in its constructor
+      this.questionText); //short form of the constructor,  this receives some input data because it gets some data in its constructor
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10), //10 device pixels of margin
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
